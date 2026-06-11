032d0717ed5446b4bd60877107c1075897b2885e
Pirjo Keskinen <pirjo.keskinen@tuni.fi>
2026-06-11 18:02:13 +0300

    feat: add main menu and scene navigation

---
+++ Scripts/UI/MainMenu.cs
@@ -0,0 +1,33 @@
+using Godot;
+
+public partial class MainMenu : Control
+{
+	private Button startButton;
+	private Button quitButton;
+
+	public override void _Ready()
+	{
+		startButton = GetNode<Button>(
+            "CenterContainer/VBoxContainer/StartButton"
+		);
+
+		quitButton = GetNode<Button>(
+            "CenterContainer/VBoxContainer/QuitButton"
+		);
+
+		startButton.Pressed += OnStartPressed;
+		quitButton.Pressed += OnQuitPressed;
+	}
+
+	private void OnStartPressed()
+	{
+		GetTree().ChangeSceneToFile(
+            "res://Scenes/Gameplay/Main.tscn"
+		);
+	}
+
+	private void OnQuitPressed()
+	{
+		GetTree().Quit();
+	}
+}
